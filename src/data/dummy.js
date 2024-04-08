a43830ec2519f04bbafd60ef475cd32a4afcc2bc
Mangesh98 <mangeshraje55555@gmail.com>
2024-04-08 20:07:41 +0530

    Updated  -title  -page name

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -488,7 +488,7 @@ export const links = [
         icon: <AiOutlineShoppingCart />,
       },
       {
-        name: 'employees',
+        name: 'stocks',
         icon: <IoMdContacts />,
       },
       {
